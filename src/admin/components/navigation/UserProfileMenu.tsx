dbe2fc02ffacd681f6bdf4bf0fe0a676b01efe17
st-miki <mikiengida52@gmail.com>
2025-03-13 20:21:59 +0300

    refactor: standardize SVG stroke properties and update column definitions in settings list for consistency

--- src/admin/components/navigation/UserProfileMenu.tsx
+++ src/admin/components/navigation/UserProfileMenu.tsx
@@ -58,8 +58,8 @@ const Icon = () => {
         aria-hidden="true"
         className="h-4">
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M8.25 4.5l7.5 7.5-7.5 7.5"></path>
       </svg>
     </div>
--- src/plugins/customer-support/admin/pages/settings/list.tsx
+++ src/plugins/customer-support/admin/pages/settings/list.tsx
@@ -42,32 +42,37 @@ export const getStaticProps: GetStaticProps = async () => {
 const SettingsColumns = [
   
       {
-          Header: "Settings Name",
-          accessor: "name",
+          id:"name",
+          header: "Settings Name",
+          accessorKey: "name",
       },
       {
-          Header: "Settings Value",
-          accessor: "value",
+          id:"value",
+          header: "Settings Value",
+          accessorKey: "value",
       },
       {
-          Header: "Created Date",
-          accessor: "created_at",
+          id:"created_at",
+          header: "Created Date",
+          accessorKey: "created_at",
           Cell: data => (
               <IMDateTableCell timestamp={data.value} />
           )
       },
       {
-          Header: "Updated Date",
-          accessor: "updated_at",
+          id:"updated_at",
+          header: "Updated Date",
+          accessorKey: "updated_at",
           Cell: data => (
               <IMDateTableCell timestamp={data.value} />
           )
-      },,
-  {
-    Header: 'Actions',
-    accessor: 'actions',
-    Cell: data => <ActionsItemView data={data} />,
-  },
+      },
+      {
+        id:"actions",
+        header: 'Actions',
+        accessorKey: 'actions',
+        Cell: data => <ActionsItemView data={data} />,
+      },
 ]
 
 function ActionsItemView(props) {
